c2d96766081bfc67c7cf46655e979a22408c0309
Janaina-Costa <janainacosta.dev@gmail.com>
2023-11-15 12:01:47 -0300

    ...

--- src/components/MainApplication/index.tsx
+++ src/components/MainApplication/index.tsx
@@ -1,13 +1,32 @@
 import { Form } from '../Form'
 import './style.css'
+import cifrao from '../../../public/icons/cifrao.svg'
+import { useState } from 'react'
 
 export const MainApplication=()=>{
+    const[isOnfocus, setIsOnFocus] =useState<boolean>(false)
+    
+    const handleOnFocus=()=>{ 
+        setIsOnFocus(true)
+        console.log('entrei', isOnfocus);
+    }
+    const handleOnBlur=()=>{
+        setIsOnFocus(false)
+        console.log('sai', isOnfocus);
+    }
+
     return(
         <main className="main-container">
             <Form
-             icon='' 
-             onBlur={()=>console.log('uiii')} 
-             onFocus={()=>console.log('')} type='number'/>
+             icon={cifrao} 
+             alt='Ícone de dinheiro'
+             onBlur={handleOnBlur} 
+             onFocus={handleOnFocus} 
+             type='number'
+             onChange={()=>console.log('')}
+             title='Conta'
+             isOnFocused={isOnfocus}
+             />
 
 
         </main>
