2e0c66a494986c1b97cf3fd24099db2668a44a7c
Janaina-Costa <janainacosta.dev@gmail.com>
2023-11-15 12:01:41 -0300

    Cria componente de input da conta Ajusta variaveis de css Cria lógica para focus do input

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,21 +1,25 @@
 import { FormHTMLAttributes } from "react"
+import './style.css'
 
 interface IProps extends FormHTMLAttributes<HTMLFormElement>{
     title:string,
     icon?:string,
     type:string,
+    alt:string,
+    isOnFocused:boolean,
     onChange:()=>void,
     onBlur:()=>void,
     onFocus:()=>void,
 }
 
-export const Form = ({title, placeholder,icon, onChange, onSubmit, onBlur, onFocus, type='text' }:IProps)=>{
+export const Form = ({title,alt,isOnFocused, placeholder,icon, onChange, onSubmit, onBlur, onFocus, type='text' }:IProps)=>{
     return(
-        <form action="" onSubmit={onSubmit}>
-            <label htmlFor="">{title}</label>
-           <div className="wrapper-input">
-            {icon}
+        <form action="" onSubmit={onSubmit} className="containerForm">
+            <label htmlFor="input-bill">{title}</label>
+           <div className={`wrapper-input ${isOnFocused? 'input-focus':''}`}>
+            <img src={icon} alt={alt} />
             <input 
+                id="input-bill"
                 type={type} 
                 placeholder={placeholder} 
                 onChange={onChange} 
