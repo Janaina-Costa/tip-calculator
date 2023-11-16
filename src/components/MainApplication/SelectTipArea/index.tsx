71695528f8ccaabf18065f6a85dd28c220dd4d01
Janaina-Costa <janainacosta.dev@gmail.com>
2023-11-15 23:12:24 -0300

    implementa logica da seleção dos botões

--- src/components/MainApplication/SelectTipArea/index.tsx
+++ src/components/MainApplication/SelectTipArea/index.tsx
@@ -1,24 +1,50 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Button } from '../../Button'
 import './style.css'
 import { tipData } from '../../../data/tipData'
 import { Form } from '../../Form'
+import  { useState } from 'react'
 
 interface IProps {
     onGetValueTip: (value: number) => void
 }
+interface ITipProps{
+    id:string
+    tip:string,
+    isSelected:boolean
+}
+
 
 export const SelectTipArea = ({ onGetValueTip }: IProps) => {
+    const[dataTip, setDataTip] = useState<ITipProps[]>(tipData)
+
     const handleKeyUp = (valueInput: number) => {
         onGetValueTip(valueInput)
 
     }
+
+    const handleButtonClick = (id:string)=>{
+        
+     setDataTip(prev=>{       
+     return  prev.map(item=> item.id === id? {...item, isSelected:!item.isSelected}:item)
+     })
+    
+    Object.values(dataTip).filter(item=>{
+        if(item.id !== id && item.isSelected){
+            return setDataTip(prev=>prev.map(tip=>tip.id !== id && tip.isSelected?{...tip, isSelected:!tip.isSelected}:tip)
+    )}
+    })
+
+}
+       
+    
     return (
         <div className='container-tip'>
             <p className='container-tip-title'>Selecione a gorjeta %</p>
             <div className="wrapper-tip-button">
-                {tipData.map(tip => (
+                {dataTip.map(tip => (
                     <>
-                        <Button text={tip.tip} isSelected={tip.isSelected} />
+                        <Button id={tip.id} text={tip.tip} isSelected={tip.isSelected} onClick={()=>handleButtonClick(tip.id)} name={tip.tip} />
                     </>
                 ))}
                 <Form placeholder='Custom' type='number' onGetValue={handleKeyUp} />
